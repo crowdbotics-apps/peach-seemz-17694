118b7d3e3933b2d2441e364aa6e72ba2f2bb99a2
Crowdbotics <team@crowdbotics.com>
2020-06-02 09:43:48 +0000

    Update models

--- backend/peach_seemz_17694/wsgi.py
+++ backend/peach_seemz_17694/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'peach_seemz_17694.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "peach_seemz_17694.settings")
 
 application = get_wsgi_application()
